d3dfeb4493f143e956b5b228e67565700906fb03
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-10-21 20:16:31 -0300

    feat(src/insights.py): implementa função filter_by_job_indutry

--- src/insights.py
+++ src/insights.py
@@ -75,7 +75,8 @@ def filter_by_industry(jobs, industry):
     list
         List of jobs with provided industry
     """
-    return []
+    jobs_by_industry = [job for job in jobs if job["industry"] == industry]
+    return jobs_by_industry
 
 
 def get_max_salary(path):

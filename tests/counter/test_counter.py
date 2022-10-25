69f0856bd5bc43806cc92c0b13a1abf5d03f5541
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-10-25 19:30:54 -0300

    test(tests/counter): implementa test da função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("src/jobs.csv", "front end") == 13
+    assert count_ocurrences("src/jobs.csv", "Front END") == 13

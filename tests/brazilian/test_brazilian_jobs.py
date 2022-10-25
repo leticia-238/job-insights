cf4136fa99f122e4240a19f48dd629df6ef27cde
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-10-25 20:36:21 -0300

    test(tests/brazilian): implementa test da função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    list_of_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert len(list_of_jobs) == 15
+
+    for job in list_of_jobs:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
+        assert "titulo" not in job
+        assert "salario" not in job
+        assert "tipo" not in job

0a549220c24661a0ae7e53fc4837d85e6920d1df
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-07 12:06:42 -0300

    test(tests/sorting): implementa test da função sort_by

---
+++ tests/sorting/data.py
@@ -0,0 +1,107 @@
+jobs = [
+    {
+        "job_title": "Senior Salesforce Developer",
+        "min_salary": "44587",
+        "max_salary": "82162",
+        "date_posted": "2020-05-08",
+    },
+    {
+        "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+        "min_salary": "125410",
+        "max_salary": "212901",
+        "date_posted": "2020-04-28",
+    },
+    {
+        "job_title": "Emergency Veterinarian - NYC",
+        "min_salary": "94715",
+        "max_salary": "103279",
+        "date_posted": "2020-05-05",
+    },
+    {
+        "job_title": "ABA Therapist",
+        "min_salary": "20000",
+        "max_salary": "35000",
+        "date_posted": "2020-05-07",
+    },
+]
+
+jobs_sorted_by_min_salary = [
+    {
+        "job_title": "ABA Therapist",
+        "min_salary": "20000",
+        "max_salary": "35000",
+        "date_posted": "2020-05-07",
+    },
+    {
+        "job_title": "Senior Salesforce Developer",
+        "min_salary": "44587",
+        "max_salary": "82162",
+        "date_posted": "2020-05-08",
+    },
+    {
+        "job_title": "Emergency Veterinarian - NYC",
+        "min_salary": "94715",
+        "max_salary": "103279",
+        "date_posted": "2020-05-05",
+    },
+    {
+        "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+        "min_salary": "125410",
+        "max_salary": "212901",
+        "date_posted": "2020-04-28",
+    },
+]
+
+jobs_sorted_by_max_salary = [
+    {
+        "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+        "min_salary": "125410",
+        "max_salary": "212901",
+        "date_posted": "2020-04-28",
+    },
+    {
+        "job_title": "Emergency Veterinarian - NYC",
+        "min_salary": "94715",
+        "max_salary": "103279",
+        "date_posted": "2020-05-05",
+    },
+    {
+        "job_title": "Senior Salesforce Developer",
+        "min_salary": "44587",
+        "max_salary": "82162",
+        "date_posted": "2020-05-08",
+    },
+    {
+        "job_title": "ABA Therapist",
+        "min_salary": "20000",
+        "max_salary": "35000",
+        "date_posted": "2020-05-07",
+    },
+]
+
+jobs_sorted_by_date_posted = [
+    {
+        "job_title": "Senior Salesforce Developer",
+        "min_salary": "44587",
+        "max_salary": "82162",
+        "date_posted": "2020-05-08",
+    },
+    {
+        "job_title": "ABA Therapist",
+        "min_salary": "20000",
+        "max_salary": "35000",
+        "date_posted": "2020-05-07",
+    },
+    {
+        "job_title": "Emergency Veterinarian - NYC",
+        "min_salary": "94715",
+        "max_salary": "103279",
+        "date_posted": "2020-05-05",
+    },
+    {
+        "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+        "min_salary": "125410",
+        "max_salary": "212901",
+        "date_posted": "2020-04-28",
+    },
+]
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,16 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from tests.sorting.data import (
+    jobs,
+    jobs_sorted_by_date_posted,
+    jobs_sorted_by_max_salary,
+    jobs_sorted_by_min_salary,
+)
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "min_salary")
+    assert jobs == jobs_sorted_by_min_salary
+    sort_by(jobs, "max_salary")
+    assert jobs == jobs_sorted_by_max_salary
+    sort_by(jobs, "date_posted")
+    assert jobs == jobs_sorted_by_date_posted
